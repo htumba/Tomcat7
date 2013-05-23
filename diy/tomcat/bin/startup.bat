e807c8803b01185a4c450f5f72c044b04c6d22cf
unknown <htumbaa@tomcat.sistema10.com>
2013-05-23 17:03:18 -0500

    commit start and stop

--- diy/tomcat/bin/startup.bat
+++ diy/tomcat/bin/startup.bat
@@ -9,7 +9,7 @@ rem
 rem     http://www.apache.org/licenses/LICENSE-2.0
 rem
 rem Unless required by applicable law or agreed to in writing, software
-rem distributed under the License is distributed on an "AS IS" BASIS,
+rem distributed under the License is distributed on an "AS IS" BASIS
 rem WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 rem See the License for the specific language governing permissions and
 rem limitations under the License.
